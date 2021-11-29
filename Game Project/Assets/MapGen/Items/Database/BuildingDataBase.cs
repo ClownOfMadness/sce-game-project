52edcc7bc92c23c78d37194aba6980a3b2a8ad59
ClownOfMadness <52707645+ClownOfMadness@users.noreply.github.com>
2021-11-29 15:35:14 +0200

    Temp(Project): Cleaned up the project

--- Game Project/Assets/MapGen/Items/Database/BuildingDataBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new DataBase", menuName = "Assets/Databases/Building Database")]
-public class BuildingDataBase : ScriptableObject
-{
-    public List<Building> BuildingList;
-}
