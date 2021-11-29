cc920e4eee58bb920a23e71836b4a1792d4fae13
MichaelVayntrub <helicam7451623@gmail.com>
2021-11-29 21:51:52 +0200

    fix(Hackathon): "fixed the generator map test"
    the tests in map generator are working noq

--- Game Project/Assets/Tests/map_gen_test.cs
+++ Game Project/Assets/Tests/map_gen_test.cs
@@ -4,52 +4,60 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class map_gen_test
+public class map_gen_test: MonoBehaviour
 {
+    //Test is the size of the map is correct.
     [Test]
     public void tile_map_size()
     {
-        MapGen map = new MapGen();
+        MapGen map = FindObjectOfType<MapGen>();
         int size = map.mapSize;
         map.generateMap();
+        
         Assert.AreEqual(size * size, map.transform.childCount);
     }
 
+    //Test if the validity of the position.
     [Test]
     public void check_valid_position()
     {
-        MapGen map = new MapGen();
-        MapGen.TerrainType tile;
-        tile.height = 0.40f;
+        MapGen map = FindObjectOfType<MapGen>();
+        MapGen.TerrainType tile = new MapGen.TerrainType();
 
-        //Assert.IsTrue(map.CheckValidPos(tile));
+        tile.height = 0.40f;// Valid height is between 0.35 and 0.6
+        Assert.IsTrue(map.CheckValidPos(tile));
+        tile.height = 0.7f;
+        Assert.IsFalse(map.CheckValidPos(tile));
     }
 
+    //Test if the map fully removed.
     [Test]
     public void destroy_tile_map()
     {
-        MapGen map = new MapGen();
+        MapGen map = FindObjectOfType<MapGen>();
         map.generateMap();
         map.deleteTileMap();
 
         Assert.IsTrue(map.transform.childCount == 0);
     }
     
+    //Test if the returned starting point is in the valid range.
     [Test]
     public void is_starting_pos_legit()
     {
-        MapGen map = new MapGen();
-        //map.generateMap();
-        int size = map.mapSize;
-        Dictionary<int, Vector2Int> PosiblePos = new Dictionary<int, Vector2Int>();
-        PosiblePos.Add(0, new Vector2Int(Random.Range(0,size), Random.Range(0, size)));
-        PosiblePos.Add(1, new Vector2Int(0, 0));
-        PosiblePos.Add(2, new Vector2Int(size, size));
+        MapGen map = FindObjectOfType<MapGen>();
+        map.generateMap();
+        int size = map.mapSize, count = 0;
 
-        // Vector3 pos = map.PlaceStartPos(PosiblePos);
-        Vector3 pos = new Vector3(10, 1, 10);
-        Assert.AreEqual(0, 0);
-        //Assert.IsTrue(pos.x <= size * 10 && pos.z <= size * 10);
+        //Create a test dictionery.
+        Dictionary<int, Vector2Int> PosiblePos = new Dictionary<int, Vector2Int>();
+        for(int y = 0; y < size; y++)
+            for(int x = 0; x < size; x++)
+                PosiblePos.Add(count++, new Vector2Int(x, y));
+
+        Vector3 pos = map.PlaceStartPos(PosiblePos);
+
+        Assert.IsTrue(pos.x <= size * 10 && pos.z <= size * 10);
         Assert.IsTrue(pos.x >= 0 && pos.z >= 0);
     }
 }
