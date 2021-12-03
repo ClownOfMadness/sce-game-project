c64670b18f6b276f9cc7975afadf8312e48e7bef
ClownOfMadness <52707645+ClownOfMadness@users.noreply.github.com>
2021-12-03 10:51:36 +0200

    Hotfix(Units): Fixed unit instantiate

--- Game Project/Assets/Scripts/Units & Player/UnitList.cs
+++ Game Project/Assets/Scripts/Units & Player/UnitList.cs
@@ -6,18 +6,27 @@ using UnityEngine;
 public class UnitList : MonoBehaviour
 {
     [Serializable]
-    public struct Units
+    public struct Units // This is a struct for connecting prefab to its holder
     {
         public GameObject unitPrefab;
         public GameObject unitGroup;
     }
 
     public Units[] units;
+    // [Int - Unit]
+    // 0 - Peasant
 
-    private Quaternion rotation = Quaternion.Euler(Vector3.zero);
+    private Quaternion rotation = Quaternion.Euler(Vector3.zero); // Default unit rotation
 
-    public void AddUnit(Vector3 location, int unit)
+    public void AddUnit(Vector3 location, int unit) // Creates the unit in a given location
     {
-        Instantiate(units[0].unitPrefab, location, rotation, units[0].unitGroup.transform);
+        if (unit < 0 || unit > units.Length)
+        {
+            Debug.LogError("You have given wrong int to the AddUnit function");
+        }
+        else
+        {
+            Instantiate(units[unit].unitPrefab, location, rotation, units[unit].unitGroup.transform);
+        }
     }
 }
