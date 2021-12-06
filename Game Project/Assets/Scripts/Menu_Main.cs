05e99815b9275948b610fa65168e122d3e973305
Len <elena24c@gmail.com>
2021-12-06 18:23:11 +0200

    Update(Menu): Added more windows

--- Game Project/Assets/Scripts/Menu_Main.cs
+++ Game Project/Assets/Scripts/Menu_Main.cs
@@ -13,6 +13,12 @@ public class Menu_Main : MonoBehaviour
     public void ExitGame()
     {
         Debug.Log("Exit");
-        Application.Quit();
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
