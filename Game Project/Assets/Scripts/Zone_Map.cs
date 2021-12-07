c497cc7e6bd441385c7d2dd29064894f0ce41595
Len <elena24c@gmail.com>
2021-12-07 18:43:00 +0200

    Update(Cards): Some card changes

--- Game Project/Assets/Scripts/Zone_Map.cs
+++ Game Project/Assets/Scripts/Zone_Map.cs
@@ -9,13 +9,12 @@ public class Zone_Map : Zone_Behaviour
     void Awake()
     {
         Size = 1;       //max Zone size 
-        Tiles = FindObjectOfType<Player_SpawnBuilding>();      //connection to use SpawnBuilding functions
+        Tiles = FindObjectOfType<Player_SpawnBuilding>();   //connection to use SpawnBuilding functions
     }
     public override void EventDrop(Card_Drag cardObject)
     {
-        if (this.transform.childCount == this.Size)     //failsafe - verifying that there's an object in the Zone
+        if (this.transform.childCount == this.Size)         //failsafe - verifying that there's an object in the Zone
         {
-
             if (Tiles.Spawn(cardObject.card.buildingPrefab, selectedTile))  //spawn the card's building on the tile that's under the pointer
             {  
                 GameObject placeholder = cardObject.placeholder;            //get the card's placeholder in the Hand to delete it
@@ -23,6 +22,7 @@ public class Zone_Map : Zone_Behaviour
                 cardObject.gameObject.SetActive(false);                     //hide in scene (can be replaced with Destroy function)
                 Size++;                                                     //needed because the cards get hidden
             }
+            //return to hand??
         }
     }
 }
