ea176298f773b25d364742700129924881d94714
MichaelVayntrub <helicam7451623@gmail.com>
2021-12-12 21:46:11 +0200

    Development(Menu): "Loading screen"
    Added running peasent when game loaded.

--- Game Project/Assets/Scripts/Menu_Main.cs
+++ Game Project/Assets/Scripts/Menu_Main.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu_Main : MonoBehaviour
 {
+    public GameObject LoadScreen;
+    AsyncOperation AsyOpe = null;
+
     public void NewGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        LoadScreen.SetActive(true);
+        StartCoroutine(LoadAsynchronic());
+    }
+
+    IEnumerator LoadAsynchronic()
+    {
+        for (int i = 1; i < 40; i++)
+        {
+            LoadScreen.GetComponent<Image>().sprite = LoadScreen.GetComponent<SpriteRenderer>().sprite;
+            yield return new WaitForSeconds(0.05f);
+        }
+        SceneManager.LoadSceneAsync("SampleScene");
+
+        yield return null;
     }
 
     public void ExitGame()
