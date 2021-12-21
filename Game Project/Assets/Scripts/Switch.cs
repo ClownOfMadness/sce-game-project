87ff5b993a9f896701bcc58882ec717e5b6955a1
Bjaui <barmocha7@gmail.com>
2021-12-21 18:15:40 +0200

    Update(PauseMenu): Added Tutorial

---
+++ Game Project/Assets/Scripts/Switch.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour
+{
+
+    public GameObject[] background;
+    int index;
+
+    void Start()
+    {
+        index = 0;
+    }
+
+
+    void Update()
+    {
+
+        if (index < 0)
+            index = 0;
+
+        if (index == 0)
+        {
+            Time.timeScale = 0f;
+            background[0].gameObject.SetActive(true);
+        }
+
+    }
+
+    public void Next()
+    {
+        if (index<11) {
+            index += 1;
+            background[index - 1].gameObject.SetActive(false);
+            background[index].gameObject.SetActive(true);
+        }
+        else 
+        {
+            this.gameObject.SetActive(false);
+            background[index].gameObject.SetActive(false);
+            background[0].gameObject.SetActive(true);
+            index = 0;
+            Time.timeScale = 1f;
+        }
+        Debug.Log(index);
+    }
+
+    public void Previous()
+    {
+        if (index > 0) { 
+        index -= 1;
+        background[index + 1].gameObject.SetActive(false);
+        background[index].gameObject.SetActive(true);
+        }
+        Debug.Log(index);
+    }
+
+
+}
