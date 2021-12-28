d580f298ac764eb45bc4fc7ded009f871a1c4f37
MichaelVayntrub <helicam7451623@gmail.com>
2021-12-29 00:12:23 +0200

    Fix(SaveSystem): "Removed a little bug"

--- Game Project/Assets/Scripts/IO_Files.cs
+++ Game Project/Assets/Scripts/IO_Files.cs
@@ -21,14 +21,14 @@ public static class IO_Files
         }
         return data;
     }
-    public static Map_Display.Data_Player ReadData(string path)  //load Data_Player from file
+    public static Data_Player ReadData(string path)  //load Data_Player from file
     {
-        Map_Display.Data_Player data = null;
+        Data_Player data = null;
         if (File.Exists(path))
         {
             BinaryFormatter formatter = GetBinaryFormatter();
             FileStream stream = new FileStream(path, FileMode.Open);
-            data = formatter.Deserialize(stream) as Map_Display.Data_Player;
+            data = formatter.Deserialize(stream) as Data_Player;
             stream.Close();     //opened files must be closed when done with
         }
         return data;
@@ -43,7 +43,7 @@ public static class IO_Files
         stream.Close();         //opened files must be closed when done with
     }
 
-    public static void WriteData(string path, Map_Display.Data_Player data)  //save the player's data to file
+    public static void WriteData(string path, Data_Player data)  //save the player's data to file
     {
         Debug.Log("Attempting to create");
         BinaryFormatter formatter = GetBinaryFormatter();
