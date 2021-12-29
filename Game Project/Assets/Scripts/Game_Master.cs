d6b7e1d09c370a805fc9bfa9a6b0ad3251604ed4
Len <elena24c@gmail.com>
2021-12-29 17:58:59 +0200

    Development(Parent): Implementing TimeLimit functionality

--- Game Project/Assets/Scripts/Game_Master.cs
+++ Game Project/Assets/Scripts/Game_Master.cs
@@ -58,39 +58,23 @@ public class Game_Master : MonoBehaviour
         //Enemies.Game = this;
 
         //when save file is picked, everything should be loaded from save file
-        timeLimit = TimeToFloat("2:00");
-        Debug.Log(string.Format("{0:00}:{1:00}", TimeSpan.FromSeconds(timeLimit).Minutes, TimeSpan.FromMilliseconds(timeLimit).Seconds));
-        timeLeft = timeLimit;
-
         //if save file wasnt picked, run new game
         NewGame();
+
+        //[testing time limit functionality]
+        //timeLimit = TimeToFloat("00:01:10");
+        //Debug.Log(FloatToTime(timeLimit));   
+        //timeLeft = timeLimit;
+        //timeLimitSet = true;
+        //[testing time limit functionality]
     }
     void Update() //main Update, handles constant checking of parameters for the rest of the game
     {
-        //general:
+        //[General]
         if(gameLost)
         {
             //this is where a lose screen will be called
         }
-        //parent:
-        //time limit
-        if (timeLimitSet && timeLeft > 0) 
-            timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
-        {
-            //save game and exit
-        }
-        //bedtime
-        if (bedTimeSet) 
-        {
-            realTime = (float)DateTime.Now.Hour + ((float)DateTime.Now.Minute * 0.01f);
-            if (realTime > bedTime)
-            {
-                //save game and exit
-            }
-        }
-
-        //premium:
         //keymapping
         if (Input.GetKeyDown(creativeK) && (!Menu_Pause.IsPaused)) //to close and open "Creative" with keyboard as well
         {
@@ -109,6 +93,29 @@ public class Game_Master : MonoBehaviour
             Jobs.SwitchJob();
         }
         //move other key listeners here
+
+        //[Parent]
+        //time limit
+        if (timeLimitSet && timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        if (timeLimitSet && timeLeft <= 0)
+        {
+            Debug.Log(string.Format("{0} ran out", FloatToTime(timeLimit)));
+            //save game and exit
+        }
+        //bedtime
+        if (bedTimeSet) 
+        {
+            realTime = (float)DateTime.Now.Hour + ((float)DateTime.Now.Minute * 0.01f);
+            if (realTime > bedTime)
+            {
+                //save game and exit
+            }
+        }
+
+        //[Premium]
     }
     public void NewGame()   //run when new game is started
     {
@@ -125,14 +132,21 @@ public class Game_Master : MonoBehaviour
         bedTimeSet = false;
         bedTime = 0;
     }
-    public float TimeToFloat(string time)
+    public float TimeToFloat(string time)   //converts time in mm:ss format into float
     {
-        string[] times = time.Split(":"[0]);
+        string[] times = time.Split(':');
 
-        float minutes = float.Parse(times[0]);
-        float seconds = float.Parse(times[1]);
-
-        return minutes * 60 + seconds;
+        float hours = float.Parse(times[0]);
+        float minutes = float.Parse(times[1]);
+        float seconds = float.Parse(times[2]);
+        return (hours * 60 + minutes) * 60 + seconds;
+    }
+    public string FloatToTime(float time)   //converts time floats into a string
+    {
+        double hours = Math.Floor(time / (60 * 60));
+        double minutes = Math.Floor((time - hours * 60) / 60);
+        double seconds = time - minutes * 60;
+        return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
     }
     public Game_Info ExportGame()             //will be used to save the game
     {
