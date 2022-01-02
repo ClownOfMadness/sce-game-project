4598f9ff191d8981f934d053fbe6d2b0fd685cdd
MichaelVayntrub <helicam7451623@gmail.com>
2022-01-02 16:41:12 +0200

    Fix(SpawnSystem): "The night will come again in the abyss"

--- Game Project/Assets/Scripts/Map_SpawnControl.cs
+++ Game Project/Assets/Scripts/Map_SpawnControl.cs
@@ -72,6 +72,7 @@ public class Map_SpawnControl : MonoBehaviour
                     {
                         firstSpawn = GameMaster.difficulty * 2 + 1;
                         SpawnResources(0, firstSpawn); //spawn firstSpawn Abysses at random places on the map
+                        Debug.Log(firstSpawn);
                     }
                     else if (DayCount > 2 && (DayCount + reAbyss - 2) % reAbyss == 0)
                         SpawnResources(0, numAbyss); //spawn Abyss at random place on the map
@@ -106,7 +107,7 @@ public class Map_SpawnControl : MonoBehaviour
                     PosDic.Remove(randPos);
 
                 float currentHeight = Map.TileArray[x, y].GetComponent<Data_Tile>().height; //get the height from the old tile
-                GameObject fog = Map.TileArray[x, y].transform.Find("Fog").gameObject; //get the fog from the old tile
+                GameObject fog = Map.TileArray[x, y].transform.Find("Fog(Clone)").gameObject; //get the fog from the old tile
 
                 GameObject.Destroy(Map.TileArray[x, y]); //destroy the old tile
                 Map.TileArray[x, y] = Map.InstantiateTile(resources[index].prefab, new Vector3(x * 10, 1, y * 10), Map, currentHeight);
