ddf2bcb6b268969f640dd7364b2bb6051b8963b6
Bjaui <barmocha7@gmail.com>
2022-01-06 11:34:44 +0200

    Update(loadingText): fixed the loading text color again

--- Game Project/Assets/Scripts/Screen_Login.cs
+++ Game Project/Assets/Scripts/Screen_Login.cs
@@ -58,6 +58,7 @@ public class Screen_Login : MonoBehaviour
                 ErrorMessage.SetActive(true);
                 Debug.Log("Incorrect Code");
             }
+            ClearInputField();
         }
     }
 
